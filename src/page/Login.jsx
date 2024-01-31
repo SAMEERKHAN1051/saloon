0d3856b722d2988a5ad1d6410675cc09e5898682
Sameer Khan <113291729+SAMEERKHAN1051@users.noreply.github.com>
2024-01-31 20:45:17 +0500

    Update Login.jsx

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,39 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function Login() {
-    const [closebtn, setcloseBtn] = useState(false)
-    function MouseOvers() {
-        setcloseBtn(prevValue=> !prevValue)
-    }
-    function closeBtn() {
-        console.log("fjahsdf");
-    }
-    return (
-        <div className="bgSection">
-            <div className="mainLoginSection">
-                <div className="loginImg">
-                    <img src="https://images.unsplash.com/photo-1521714161819-15534968fc5f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                </div>
-                <div className="loginInput">
-                    <div className="inputText">
-                        <h3>Log in and Stay Connected</h3>
-                        <p>Our secure login process ensures the confidentiality of your information. Log in today and stay connected to your finances, anytime and anywhere.</p>
-                        <form className="loginForm">
-                            <input type="email" placeholder="Enter Email..." />
-                            <input type="password" placeholder="Enter Password..." />
-                            <div className="ForgotPassword">
-                                <p>Forgot Password?</p>
-                            </div>
-                            <button className="themeBtn">Login</button>
-                        </form>
-                        <div className="otherText">
-                            <p>Don't Have An Account?<span> Register Now</span></p>
-                            <p>By clicking Login you are agreeing with our</p>
-                            <h5>Terms of Feature</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+// export default function Login() {
+//     const [closebtn, setcloseBtn] = useState(false)
+//     function MouseOvers() {
+//         setcloseBtn(prevValue=> !prevValue)
+//     }
+//     function closeBtn() {
+//         console.log("fjahsdf");
+//     }
+//     return (
+//         <div className="bgSection">
+//             <div className="mainLoginSection">
+//                 <div className="loginImg">
+//                     <img src="https://images.unsplash.com/photo-1521714161819-15534968fc5f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+//                 </div>
+//                 <div className="loginInput">
+//                     <div className="inputText">
+//                         <h3>Log in and Stay Connected</h3>
+//                         <p>Our secure login process ensures the confidentiality of your information. Log in today and stay connected to your finances, anytime and anywhere.</p>
+//                         <form className="loginForm">
+//                             <input type="email" placeholder="Enter Email..." />
+//                             <input type="password" placeholder="Enter Password..." />
+//                             <div className="ForgotPassword">
+//                                 <p>Forgot Password?</p>
+//                             </div>
+//                             <button className="themeBtn">Login</button>
+//                         </form>
+//                         <div className="otherText">
+//                             <p>Don't Have An Account?<span> Register Now</span></p>
+//                             <p>By clicking Login you are agreeing with our</p>
+//                             <h5>Terms of Feature</h5>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
