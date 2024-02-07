b3d660cdc8347c22ffd6511c020a3325089fb80b
Sameer Khan <113291729+SAMEERKHAN1051@users.noreply.github.com>
2024-02-08 02:27:01 +0500

    Update Blog

--- src/page/components/Blog_components/Blog_Section.jsx
+++ src/page/components/Blog_components/Blog_Section.jsx
@@ -3,41 +3,6 @@ import data from '../../../json/blog.json'
 import { useState } from "react";
 
 export default function BlogSection() {
-
-    const [blogData] = useState(data)
-
-    const imgData = [
-        {
-            id: 1,
-            img: "https://images.unsplash.com/photo-1537673156864-5d2c72de7824?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "Card title",
-            description: "Some quick example text to build on the card title and make up the bulk of the card's content."
-        },
-        {
-            id: 2,
-            img: "https://images.unsplash.com/photo-1537673156864-5d2c72de7824?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "Card title",
-            description: "Some quick example text to build on the card title and make up the bulk of the card's content."
-        },
-        {
-            id: 3,
-            img: "https://images.unsplash.com/photo-1537673156864-5d2c72de7824?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "Card title",
-            description: "Some quick example text to build on the card title and make up the bulk of the card's content."
-        },
-        {
-            id: 4,
-            img: "https://images.unsplash.com/photo-1537673156864-5d2c72de7824?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "Card title",
-            description: "Some quick example text to build on the card title and make up the bulk of the card's content."
-        },
-        {
-            id: 5,
-            img: "https://images.unsplash.com/photo-1537673156864-5d2c72de7824?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "Card title",
-            description: "Some quick example text to build on the card title and make up the bulk of the card's content."
-        },
-    ]
     const card = data.map((x) => <div key={x.id} className="BlogCard">
         <img src={x.img} className="blogImg" alt="..." />
         <div className="body">
